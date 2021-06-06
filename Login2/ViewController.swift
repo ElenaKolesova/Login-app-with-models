1fc378f65352c297fb945748354e7e4981b7a23f
Elena <lenchik92-04-05@mail.ru>
2021-06-06 18:48:53 +0300

    Initial Commit

---
+++ Login2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Login2
+//
+//  Created by Elena Kolesova on 06.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
